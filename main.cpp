34c7d1e14f6deca27369b95465f57f57e7ee6f46
yj <wkgek@naver.com>
2022-05-02 09:30:34 +0900

    5월 2일 첫번째 과제

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	a = 10;
+	cout << a + 3 << endl;
+	cout << a - 3 << endl;
+	cout << a * 3 << endl;
+	cout << a / 3 << endl;
+	cout << a % 3 << endl;
+	cout << endl;
+	
+	float b = 0;
+	b = 24.3f;
+	cout << b + 3 << endl;
+	cout << b - 3 << endl;
+	cout << b * 3 << endl;
+	cout << b / 3 << endl;
+	//cout << b % 3 << endl;
+	cout << endl;
+
+	return 0;
+}
